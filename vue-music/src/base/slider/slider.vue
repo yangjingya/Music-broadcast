0d7a31f01081491d88893d61935eec0f295829ef
yangjingya <1046042127@qq.com>
2018-07-23 11:06:25 +0800

    A music broadcast

---
+++ vue-music/src/base/slider/slider.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="slider">
+        <div class="slider-group">
+            <slot>
+
+            </slot>
+        </div>
+        <div class="dots">
+
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    @import "~common/stylus/variable"
+</style>
+
+
